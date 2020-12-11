e338066a4e16c4ca154ab3ca06d91d98e5611027
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-11 22:00:27 +0300

    framer examples

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Pizza-framer-site
+
 A simple Pizza website built with React and Framer Motion.
+
+## Python learning resources
+
+#### [ 1) Python course by AlphaCodingSkills](https://www.alphacodingskills.com/python/python-introduction.php)
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
+    <motion.div className="home container"
+     animate={
+       {
+         rotateZ: 180,
+         opacity: 0.2,
+         marginTop: 200,
+       }
+     }
+    >
       <h2>
         Welcome to Pizza Joint
       </h2>
       <Link to="/base">
-        <button>
+        <motion.button
+         animate={
+           {scale: 1.2, 
+          }
+         }
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
