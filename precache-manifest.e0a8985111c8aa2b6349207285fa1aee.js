1fb7bf0fc7fac75d80850110483a9acfe6e11fdf
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-14 20:32:18 +0300

    Updates

---
+++ precache-manifest.e0a8985111c8aa2b6349207285fa1aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e084c31d13ac980366f0b650426c1f63",
+    "url": "/Pizza-framer-site/index.html"
+  },
+  {
+    "revision": "de2c1611f5779122d592",
+    "url": "/Pizza-framer-site/static/css/main.af9e611b.chunk.css"
+  },
+  {
+    "revision": "889381d836c014dd96df",
+    "url": "/Pizza-framer-site/static/js/2.424e4e62.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Pizza-framer-site/static/js/2.424e4e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2c1611f5779122d592",
+    "url": "/Pizza-framer-site/static/js/main.7cf23102.chunk.js"
+  },
+  {
+    "revision": "4e3346dc8a47da467813",
+    "url": "/Pizza-framer-site/static/js/runtime-main.82c0b8d1.js"
+  }
+]);
